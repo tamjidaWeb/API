e505efeb8cb0c926a1b63e443cfa9c1a302d4c10
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-02 21:11:12 +0600

    commited

---
+++ index.js
@@ -0,0 +1,16 @@
+function loadUsers2(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res=> res.json())
+    .then(data => displayUsers2(data))
+}
+
+function displayUsers2(data){
+    // console.log(data);
+   const ul = document.getElementById('users-list');
+   for(const user of data){
+    console.log(user.name);
+    const li = document.createElement('li');
+    li.innerText = user.name;
+    ul.appendChild(li);
+   }
+}
