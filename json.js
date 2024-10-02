448ea26adbd8faa815d6028dc41fcb49b741e21f
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-02 16:21:27 +0600

    commited

--- json.js
+++ json.js
@@ -1,5 +1,27 @@
 const user = {id: 1, name: 'gorib amir', job: 'actor'};
 //JavaScript object notation
 const stringified = JSON.stringify(user)
-console.log(user);
-console.log(stringified);
+// console.log(user);
+// console.log(stringified);
+
+// { id: 1, name: 'gorib amir', job: 'actor' }
+// {"id":1,"name":"gorib amir","job":"actor"}
+
+
+const shop = {
+    owner : 'alia',
+    address:{
+        street:'kochukhet',
+        city:'rabir',
+        country: ' bd'
+    },
+    products:['laptop','monitor','keyboard'],
+    revenue: 45000,
+    isOpen: true,
+    isNew: false,
+};
+console.log(shop)
+const shopJSON = JSON.stringify(shop);
+console.log(shopJSON);
+const shopObj = JSON.parse(shopJSON);
+console.log(shopObj);
