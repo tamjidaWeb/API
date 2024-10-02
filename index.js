9e953289a0fe682520a4767fae9838192e15810b
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-02 21:23:55 +0600

    commited

--- index.js
+++ index.js
@@ -4,13 +4,25 @@ function loadUsers2(){
     .then(data => displayUsers2(data))
 }
 
+// function displayUsers2(data){
+//     // console.log(data);
+//    const ul = document.getElementById('users-list');
+//    for(const user of data){
+//     console.log(user.name);
+//     const li = document.createElement('li');
+//     li.innerText = user.name;
+//     ul.appendChild(li);
+//    }
+// }
+
+
+
 function displayUsers2(data){
-    // console.log(data);
    const ul = document.getElementById('users-list');
    for(const user of data){
     console.log(user.name);
     const li = document.createElement('li');
-    li.innerText = user.name;
+    li.innerText = user.username;
     ul.appendChild(li);
    }
 }
