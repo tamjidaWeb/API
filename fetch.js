300ea9ca44224c0fb61f218db30323b901b5d3d4
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-02 17:39:07 +0600

    commited

--- fetch.js
+++ fetch.js
@@ -1,3 +1,18 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(json => console.log(json))
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+
+// const url = 'https://jsonplaceholder.typicode.com/todos/1';
+// fetch(url)
+// .then(Response => Response.json())
+// .then(json =>console.log(json))
+
+
+function loadData(){
+    const url = 'https://jsonplaceholder.typicode.com/todos/1';
+    fetch(url)
+.then(Response => Response.json())
+.then(json =>console.log(json))
+
+
+}
