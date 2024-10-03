0ca22f90958970aaf1e5557a926affa37986a472
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-03 09:30:46 +0600

    commited

--- posts.js
+++ posts.js
@@ -37,6 +37,27 @@ function displayPosts(posts){
 }
 loadPosts();
 
+function deletePost(){
+    fetch('https://jsonplaceholder.typicode.com/posts/1', {
+        method: 'DELETE',
+      });
+      
+}
+
+function patchPost(){
+    fetch('https://jsonplaceholder.typicode.com/posts/1', {
+        method: 'PATCH',
+        body: JSON.stringify({
+          title: 'foo',
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+}
+
 
 
 
