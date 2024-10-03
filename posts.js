22138b9eb4e37078af78acbf1e17ada7bf1294f5
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-03 08:26:32 +0600

    commited

--- posts.js
+++ posts.js
@@ -1,15 +1,48 @@
+// function loadPosts(){
+//     fetch('https://jsonplaceholder.typicode.com/posts')
+//     .then(res=> res.json())
+//     .then(data=> displayPosts(data))
+// }
+//get the element where you want to add the new elements
+
+// function displayPosts(posts){
+//     for(const post of posts){
+//         const postContainer = document.getElementById('posts-container')
+//         console.log(post)
+//     }
+
+// }
+// loadPosts();
+
 function loadPosts(){
     fetch('https://jsonplaceholder.typicode.com/posts')
     .then(res=> res.json())
-    .then(data=> displayPosts(data))
+    .then(data=>displayPosts(data))
+
 }
 //get the element where you want to add the new elements
-
 function displayPosts(posts){
+    const postContainer = document.getElementById('posts-container');
     for(const post of posts){
-        const postContainer = document.getElementById('posts-container')
+        const postDiv = document.createElement('div');
+        postDiv.classList.add('post');
         console.log(post)
+        postDiv.innerHTML = `
+        <h5> User- ${post.userId}</h5>
+        <h6>Post : title ${post.title}</h6>
+        <p> Post Description: ${post.body} </p>
+        `;
+        postContainer.appendChild(postDiv);
     }
-
 }
 loadPosts();
+
+
+
+
+
+
+
+
+
+
