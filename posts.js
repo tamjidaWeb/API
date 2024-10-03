8b00a4beb9ef9fbc6bce0509c53f67c5f3e70968
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-03 09:34:07 +0600

    commited

--- posts.js
+++ posts.js
@@ -59,6 +59,22 @@ function patchPost(){
 }
 
 
+function createPost(){
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+          title: 'foo',
+          body: 'bar',
+          userId: 1,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+}
+
 
 
 
