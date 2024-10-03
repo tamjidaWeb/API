b35e9ae5d83c26a15f21715a00ccfcc740e47839
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-03 11:29:06 +0600

    commited

--- comments.js
+++ comments.js
@@ -6,5 +6,12 @@ const loadComments = () =>{
 }
 
 const loadComments2 = async()=>{
+  try{
     const res= await fetch('https://jsonplaceholder.typicode.com/comments');
+    const data = await res.json();
+    console.log(data);
+  }
+  catch(error){
+    console.error('data load error')
+  }
 }
